b48f381717eda2f6c6f9dc0fcdebf3950e42da90
Frangersa <frangersal@outlook.com>
2020-09-07 12:38:40 -0500

    Segundo Commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@700&display=swap" rel="stylesheet">
+        
+        <!-- Styles -->
+        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/gamelist.css') }}" rel="stylesheet">
+        <!-- JS, Popper.js, and jQuery -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    </head>
+    
+    <!-- CONTENIDO -->
+    <body>
+        <div class="flex-center position-ref full-height">
+
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif 
+            <!-- CONTENIDO -->
+            @yield('content') 
+        </div>
+    </body>
+</html>
---
+++ resources/views/perfil.blade.php
@@ -0,0 +1,123 @@
+@extends('layouts.app')
+@section('content')
+<div class="content">
+    <div class="row">
+        <div class="col bg-azul-secundario" >
+        
+        </div>
+        <div class="col-9 bg-azul-primario"  >
+        
+        <div class="bg-amarillo-verde">
+                titulo 1 
+        </div>
+
+        </div>
+        
+        <div class="col bg-azul-secundario ">
+        </div>
+    </div>
+    
+    <div class="row">
+
+        <div class="col bg-azul-secundario " > </div>
+
+        <div class="col-9 bg-azul-primario">
+
+            <!-- Carusel -->
+
+            <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                </ol>
+
+                <div class="carousel-inner">
+                    
+                    <div class="carousel-item">
+                        <img src="{!! asset('images/hl2.jpg') !!}" class="d-block w-100" alt="hl2">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Second slide label</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="{!! asset('images/gow.jpg') !!}" class="d-block w-100" alt="gow">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Third slide label</h5>
+                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+
+                <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+
+            </div>
+            
+            <!-- Titulo -->
+        
+            <div class="bg-amarillo-verde">
+                <h2>Titulo 1</h2> 
+            </div>
+        
+            <div class="bg-azul-primario">
+                <div class="card" style="width: 10rem;">
+                    <img src="{!! asset('images/CVIcover.jpg') !!}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example</p>
+                        <a href="#" class="btn btn-primary">+GameList</a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Titulos -->
+
+            <div class="row">
+                <div class="col bg-amarillo-verde" >
+                    <h2>Titulo 2</h2> 
+                </div>
+                <div class="col bg-amarillo-verde ">
+                    <h2>Titulo 3</h2> 
+                </div>
+            </div>
+        
+            <div class="row">
+                <div class="col bg-azul-primario">
+                    <div class="card" style="width: 10rem;">
+                        <img src="{!! asset('images/CVIcover.jpg') !!}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example</p>
+                            <a href="#" class="btn btn-primary">+GameList</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col bg-azul-primario">
+                    <div class="card" style="width: 10rem;">
+                        <img src="{!! asset('images/CVIcover.jpg') !!}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example</p>
+                            <a href="#" class="btn btn-primary">+GameList</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br>
+        </div>
+
+        <div class="col bg-azul-secundario" > </div>
+
+    </div>
+</div>
+@endsection
